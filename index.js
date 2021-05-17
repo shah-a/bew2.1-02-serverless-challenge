53afee101e973eaf3bd5fe316f623fe28a9098b2
Ali Shah <s.ashah@outlook.com>
2021-05-17 05:50:53 -0400

    [Added] routes for DynamoDB `User` resource

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const AWS = require('aws-sdk');
 
 const app = express();
 
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+app.use(express.json({ strict: false }));
+
 app.get('/', (req, res) => {
   res.send("Salaam, world! :)");
+});
+
+app.get('/users/:userId', (req, res) => {
+  const params = {
+    TableName: USERS_TABLE,
+    Key: {
+      userId: req.params.userId
+    }
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ error: "Could not get user" });
+    }
+    if (result.Item) {
+      const { userId, name } = result.Item;
+      return res.json({ userId, name });
+    }
+    return res.status(404).json({ error: "User not found" });
+  });
+});
+
+app.post('/users', (req, res) => {
+  const { userId, name } = req.body;
+  if (typeof userId !== 'string' || typeof name !== 'string') {
+    return res.status(400).json({ error: "\"userId\" and \"name\" must both be strings" })
+  }
+
+  const params = {
+    TableName = USERS_TABLE,
+    Item: {
+      userId,
+      name
+    }
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      return res.status(400).json({ error: "Could not add user" });
+    }
+    return res.json({ userId, name });
+  });
 });
 
 module.exports.handler = serverless(app);
